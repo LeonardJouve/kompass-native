37bcad22489c8fb2142dc66a18d7768e6063f80d
Léonard <leonard.jouve@gmail.com>
2023-02-10 17:53:27 +0100

    rename router

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 
 import store from '@redux/store';
 import {name as appName} from './app.json';
-import Rooter from './rooter';
+import Rooter from './router';
 
 const Index = () => (
     <Provider store={store}>
