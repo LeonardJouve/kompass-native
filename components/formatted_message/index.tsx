316f32e57b60c7b5f151d19ded5853e42512c2bb
Léonard <leonard.jouve@gmail.com>
2023-03-27 23:10:36 +0200

    remove unused dependencie

--- components/formatted_message/index.tsx
+++ components/formatted_message/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import deepEqual from 'deep-equal';
 import {Text} from '@renative/index';
 import useFormattedMessage from '@hooking/useFormattedMessage';
 import {TranslationKey} from '@typing/language';
@@ -19,6 +18,4 @@ const FormattedMessage = ({id, defaultMessage, values}: Props) => {
     );
 };
 
-const areEqual = (prevProps: Props, nextProps: Props) => deepEqual(prevProps.values, nextProps.values);
-
-export default React.memo(FormattedMessage, areEqual);
+export default React.memo(FormattedMessage);
