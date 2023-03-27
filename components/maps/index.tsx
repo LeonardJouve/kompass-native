8a321107bd7cfe56bcc1cf7e0525484d0167abc7
Léonard <leonard.jouve@gmail.com>
2023-03-27 21:27:59 +0200

    refacto permissions check

--- components/maps/index.tsx
+++ components/maps/index.tsx
@@ -1,27 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
-import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {check, request, PERMISSIONS, RESULTS, Permission} from 'react-native-permissions';
+
+const checkAndRequestPermissionsIfNeeded = async (permissions: Permission[]) => {
+    return permissions.every(async (permission) => {
+        let granted = await check(permission) === RESULTS.GRANTED;
+        if (!granted) {
+            granted = await request(permission) === RESULTS.GRANTED;
+        }
+        return granted;
+    });
+};
 
 const Maps = () => {
     const [permissionsGranted, setPermissionsGranted] = useState(false);
 
     const checkPermissions = async () => {
-        let fineLocationGranted = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION) === RESULTS.GRANTED;
-        let coarseLocationGranted = await check(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION) === RESULTS.GRANTED;
-        if (!fineLocationGranted) {
-            fineLocationGranted = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION) === RESULTS.GRANTED;
-            if (!fineLocationGranted) {
-                return setPermissionsGranted(false);
-            }
-        }
-        if (!coarseLocationGranted) {
-            coarseLocationGranted = await request(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION) === RESULTS.GRANTED;
-            if (!coarseLocationGranted) {
-                return setPermissionsGranted(false);
-            }
-        }
-        setPermissionsGranted(true);
+        const granted = await checkAndRequestPermissionsIfNeeded([
+            PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+            PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+        ]);
+        setPermissionsGranted(granted);
     };
 
     useEffect(() => {
