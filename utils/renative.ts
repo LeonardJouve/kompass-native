8298da0694e21780b0921ebba7deb5bc8e2f8460
Léonard Jouve <leonard.jouve@gmail.com>
2023-06-03 11:10:23 +0200

    change color brightness util

--- utils/renative.ts
+++ utils/renative.ts
@@ -10,3 +10,17 @@ export const getSpacings = (spacings: Spacings, values: MarginProp | PaddingProp
     }, {});
     return newValues;
 };
+
+const clamp = (value: number, min: number, max: number) => {
+    return Math.min(Math.max(value, min), max);
+};
+
+export const changeColorBrightness = (hexColor: string, brightnessDelta: number, opacity = 1) => {
+    let r = parseInt(hexColor.substring(1, 3), 16);
+    r = Math.round(clamp((1 + brightnessDelta) * r, 0, 255));
+    let g = parseInt(hexColor.substring(3, 5), 16);
+    g = Math.round(clamp((1 + brightnessDelta) * g, 0, 255));
+    let b = parseInt(hexColor.substring(5, 7), 16);
+    b = Math.round(clamp((1 + brightnessDelta) * b, 0, 255));
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+};
