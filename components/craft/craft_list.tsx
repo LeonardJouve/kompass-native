a8facc072146b71114761c02423fbd86dac9296b
Léonard Jouve <leonard.jouve@gmail.com>
2023-07-21 20:29:25 +0200

    add craft no result indicator

--- components/craft/craft_list.tsx
+++ components/craft/craft_list.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {styled} from 'styled-components/native';
 import {View} from '@renative';
 import {getCraftsByCategory} from '@redux/selectors/craft';
+import useFormattedMessage from '@hooking/useFormattedMessage';
 import CraftListCategory from '@components/craft/craft_list_category';
+import NoResultIndicator from '@components/no_result_indicator';
+import EmptyCraftIcon from '@res/empty_craft_icon.svg';
 
 const CraftList = () => {
+    const formatMessage = useFormattedMessage();
     const craftsByCategory = useSelector(getCraftsByCategory);
     const renderedCrafts = craftsByCategory.map(({category, crafts}) => (
         <CraftListCategory
@@ -14,11 +19,34 @@ const CraftList = () => {
         />
     ));
 
+    const noResultText = formatMessage({
+        id: 'components.craft_list.no_result.text',
+        defaultMessage: 'Nothing at the moment',
+    });
+
+    let noResultIndicator;
+    if (!craftsByCategory.length) {
+        noResultIndicator = (
+            <NoResultIndicator
+                icon={<StyledEmptyCraftIcon/>}
+                textVariant='primary'
+                title={noResultText}
+            />
+        );
+    }
+
     return (
-        <View>
+        <View variants={['flex']}>
             {renderedCrafts}
+            {noResultIndicator}
         </View>
     );
 };
+
+const StyledEmptyCraftIcon = styled(EmptyCraftIcon)(({theme}) => ({
+    width: 90,
+    height: 90,
+    fill: theme.colors.variants['buttonPrimary-dark-3'],
+}));
 
 export default CraftList;
